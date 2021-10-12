ba7089b137999cb0d36e97d6d6ed7f3fc24ca15f
Tobi-De <tobidegnon@protonmail.com>
2021-10-12 16:43:39 +0100

    remove ssl email config

--- config/settings/production.py
+++ config/settings/production.py
@@ -103,7 +103,6 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
 
 # LOGGING
 # ------------------------------------------------------------------------------
